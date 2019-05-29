58a357d52d59c457e1e7fdecc6098e4a6e04fdab
Alex <avseenkov_a@mail.ru>
2019-05-29 20:37:16 +0200

    решение 2 задачи 2 модуля

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,25 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.tmpChunks = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const indxEOL = chunk.toString().indexOf(os.EOL);
+    if (indxEOL === -1) {
+      this.tmpChunks += chunk.toString();
+    } else {
+      const arrChunk = (this.tmpChunks + chunk.toString()).split(os.EOL);
+
+      arrChunk.forEach((chunk) => {
+        this.push(chunk);
+      });
+    }
+    callback();
   }
 
   _flush(callback) {
+    callback();
   }
 }
 
