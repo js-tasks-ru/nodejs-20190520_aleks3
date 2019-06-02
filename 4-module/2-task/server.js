e1ab354971ded6318b4558a0cb19d78bc4fb4e39
Alex <avseenkov_a@mail.ru>
2019-06-02 14:51:27 +0200

    добавлено решение 2 задачи 4 модуля

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,23 +1,56 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
-
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 const server = new http.Server();
+
+const handleError = (err, res) => {
+  res.end('error');
+};
+
+const handleErrorLimitStream = (err, res, filepath) => {
+  res.statusCode = 413;
+  fs.unlink(filepath, () => {});
+  res.end();
+};
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
-    case 'POST':
+  fs.stat(filepath, (err, stats) => {
+    if (stats) {
+      res.statusCode = 409;
+      res.end('file is exists');
+      return;
+    }
 
-      break;
+    switch (req.method) {
+      case 'POST':
+        const stream = req
+            .pipe(new LimitSizeStream({limit: 1048576}))
+            .on('error', (err) => handleErrorLimitStream(err, res, filepath));
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+        stream
+            .pipe(fs.createWriteStream(filepath))
+            .on('error', (err) => handleError(err, res))
+            .on('close', () => {
+              res.statusCode = 201;
+              res.end();
+            });
+
+        break;
+      default:
+        res.statusCode = 501;
+        res.end('Not implemented');
+    }
+    req.on('close', () => {
+      if (res.finished) return;
+      fs.unlink(filepath, (err) => {});
+    });
+  });
 });
 
 module.exports = server;
