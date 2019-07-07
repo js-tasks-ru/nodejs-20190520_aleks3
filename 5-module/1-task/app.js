8cc9be00097d3a1d6c471ccede0d36de0115b57a
Alex <avseenkov_a@mail.ru>
2019-07-07 13:04:04 +0200

    solution 5-1

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -7,10 +7,34 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const clients = new Set();
+
+const getMessage = (ctx) =>
+  new Promise((resolve, reject) => {
+    clients.add(resolve);
+
+    ctx.res.on('close', function() {
+      clients.delete(resolve);
+      resolve();
+    });
+  });
+
 router.get('/subscribe', async (ctx, next) => {
+  const message = await getMessage(ctx);
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+
+  if (!message) {
+    ctx.throw(400, 'required field `message` is missing');
+  }
+
+  clients.forEach((i) => i(message));
+  clients.clear();
+
+  ctx.body = 'ok';
 });
 
 app.use(router.routes());
